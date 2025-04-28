77b752341466983178edbfb526e16faf2daffc09
aniciya777 <kisaost777@gmail.com>
2025-04-28 13:29:40 +0500

    Add rectangle and thore forms on Automation

--- cellular_automation/automation.py
+++ cellular_automation/automation.py
@@ -6,11 +6,32 @@ class Automation:
                  rows: int,
                  cols: int,
                  agent_class: type[Agent],
+                 fake_agent: Agent | None = None,
                  ) -> None:
         self._rows = rows
         self._cols = cols
         self._status = False
         self._agent_class = agent_class
+        self._fake_agent = fake_agent
+        self._grid: list[list[Agent]] = [
+            [agent_class(self, row, col) for col in range(self._cols)]
+            for row in range(self._rows)
+        ]
+
+    def create_agent(self, row: int, col: int, *args, **kwargs) -> Agent | None:
+        if 0 <= row < self._rows and 0 <= col < self._cols:
+            agent = self._agent_class(self, row, col, *args, **kwargs)
+            self._grid[row][col] = agent
+            return agent
+        return self._fake_agent
+
+    def get(self, row: int, col: int) -> Agent | None:
+        if 0 <= row < self._rows and 0 <= col < self._cols:
+            return self._grid[row][col]
+        return self._fake_agent
+
+    def __call__(self, row: int, col: int) -> Agent | None:
+        return self.get(row, col)
 
     @property
     def status(self) -> bool:
@@ -21,3 +42,15 @@ class Automation:
 
     def stop(self) -> None:
         self._status = False
+
+
+class ThoreMixin:
+    def create_agent(self, row: int, col: int, *args, **kwargs) -> Agent | None:
+        row %= self._rows
+        col %= self._cols
+        agent = self._agent_class(self, row, col, *args, **kwargs)
+        self._grid[row][col] = agent
+        return agent
+
+    def get(self, row: int, col: int) -> Agent | None:
+        return self._grid[row % self._rows][col % self._cols]
