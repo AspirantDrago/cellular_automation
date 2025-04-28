8425c403d6aa39d52a53138c7bfa409b91610131
aniciya777 <kisaost777@gmail.com>
2025-04-28 12:47:58 +0500

    Add config.py

---
+++ config.py
@@ -0,0 +1,30 @@
+import dataclasses
+
+import pygame as pg
+
+
+@dataclasses.dataclass
+class Padding:
+    top: int
+    right: int
+    bottom: int
+    left: int
+
+
+class Config:
+    # размеры
+    COLUMNS = 100
+    ROWS = 100
+
+    CELL_SIZE = 5
+    TITLE = 'Game of Life'
+
+    PADDING = Padding(10, 10, 10, 10)
+
+    WIDTH = COLUMNS * CELL_SIZE + PADDING.left + PADDING.right
+    HEIGHT = COLUMNS * CELL_SIZE + PADDING.top + PADDING.bottom
+    SIZE = (WIDTH, HEIGHT)
+
+    BACKGROUND_COLOR = pg.color.Color(255, 255, 255)
+
+    FPS = 60
